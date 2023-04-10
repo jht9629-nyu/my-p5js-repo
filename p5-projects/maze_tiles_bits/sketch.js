b00d4a13cdc0cd7688e9ccc412cd8c38217f53fe
John Henry Thompson <jht1493@gmail.com>
2023-04-10 10:32:54 -0400

    do_random

--- p5-projects/maze_tiles_bits/sketch.js
+++ p5-projects/maze_tiles_bits/sketch.js
@@ -2,46 +2,51 @@
 // maze tiles bits
 
 let len = 40;
+let a_strokeWeight = 0.3;
+
 let a_now = [];
 let a_next = [];
+let a_random = [];
+let a_target;
 
 let draw_step;
 let maze_step_period = 1.0;
 let maze_pause_period = 0.5;
+let do_random = 1;
 
 let a_timer;
-
-let a_strokeWeight = 8;
 
 function setup() {
   createCanvas(400, 400);
   noFill();
-  strokeWeight(a_strokeWeight);
+  strokeWeight(len * a_strokeWeight);
 
   fill_zero(a_now);
   fill_zero(a_next);
+  fill_zero(a_random);
+
   fill_incr(a_next);
 
-  a_timer = new SecondsTimer(maze_step_period);
+  a_target = a_next;
 
+  a_timer = new SecondsTimer();
   draw_step = draw_maze_step;
+  a_timer.setPeriod(maze_step_period);
 }
 
 function draw() {
   draw_step();
 }
 
-function draw_maze_step() {
+function draw_maze() {
   background(220);
-
   let a_angle = HALF_PI * (a_timer.lapse() / a_timer.period);
-
   let half = len / 2;
   let index = 0;
   for (let y = 0; y < height; y += len) {
     for (let x = 0; x < width; x += len) {
       let now = a_now[index];
-      let target = a_next[index];
+      let target = a_target[index];
       let angle = now == target ? 0 : a_angle;
       if (now) {
         drawLeft(x, y, half, angle);
@@ -51,6 +56,10 @@ function draw_maze_step() {
       index++;
     }
   }
+}
+
+function draw_maze_step() {
+  draw_maze();
 
   if (a_timer.arrived()) {
     fill_incr(a_now);
@@ -62,6 +71,53 @@ function draw_maze_step() {
 }
 
 function draw_maze_pause() {
+  if (a_timer.arrived()) {
+    draw_step = do_random ? draw_maze_random : draw_maze_step;
+    a_timer.setPeriod(maze_step_period);
+  }
+}
+
+function draw_maze_random() {
+  fill_random(a_random);
+  a_target = a_random;
+
+  draw_step = draw_maze_random_step;
+  a_timer.setPeriod(maze_step_period);
+}
+
+function draw_maze_random_step() {
+  draw_maze();
+
+  if (a_timer.arrived()) {
+    let now_save = a_now;
+    a_now = a_target;
+    a_target = now_save;
+
+    draw_step = draw_maze_random_pause;
+    a_timer.setPeriod(maze_pause_period);
+  }
+}
+
+function draw_maze_random_pause() {
+  if (a_timer.arrived()) {
+    draw_step = draw_maze_random_pause_step;
+    a_timer.setPeriod(maze_step_period);
+  }
+}
+
+function draw_maze_random_pause_step() {
+  draw_maze();
+
+  if (a_timer.arrived()) {
+    a_now = a_target;
+    a_target = a_next;
+
+    draw_step = draw_maze_random_pause2;
+    a_timer.setPeriod(maze_pause_period);
+  }
+}
+
+function draw_maze_random_pause2() {
   if (a_timer.arrived()) {
     draw_step = draw_maze_step;
     a_timer.setPeriod(maze_step_period);
