6baf4cd17528ac4ce766e926433b4c20015e2156
John Henry Thompson <jht1493@gmail.com>
2023-04-11 12:39:41 -0400

    Create sketch.js

---
+++ p5-projects/spiral_walk/sketch.js
@@ -0,0 +1,80 @@
+// https://editor.p5js.org/jht9629-gmail/sketches/zaAsh0DZt
+// spiral walk
+
+let my = {};
+
+function setup() {
+  createCanvas(400, 400);
+
+  my.L = int(width / 2);
+  my.T = int(height / 2);
+  my.R = my.L;
+  my.B = my.T;
+  my.px = my.L;
+  my.py = my.T;
+  my.d = 10;
+
+  grow_box();
+  grow_box();
+
+  background(220);
+
+  my.pts = [];
+  let more = 20;
+  while (more) {
+    // move up
+    my.nx = my.px;
+    my.ny = my.py - my.d;
+    my_line({ dx: 0, dy: 0 });
+
+    // move right
+    my.nx = my.px + my.d;
+    while (my.nx < my.R) {
+      my_line({ dx: my.d, dy: 0 });
+    }
+    my.nx -= my.d;
+
+    // move down
+    my.ny += my.d;
+    while (my.ny < my.B) {
+      my_line(0, my.d);
+    }
+    my.ny -= my.d;
+
+    // move left
+    my.nx = my.px - my.d;
+    while (my.nx > my.L) {
+      my_line(-my.d, 0);
+    }
+
+    my.nx += my.d;
+
+    // move up
+    my.ny -= my.d;
+    while (my.ny > my.T) {
+      my_line(0, -my.d);
+    }
+    my.ny += my.d;
+
+    grow_box();
+
+    more--;
+  }
+  // console.log(my.pts)
+}
+
+function my_line({ dx, dy }) {
+  my.pts.push([my.px, my.py]);
+  line(my.nx, my.ny, my.px, my.py);
+  my.px = my.nx;
+  my.py = my.ny;
+  my.nx += dx;
+  my.ny += dy;
+}
+
+function grow_box() {
+  my.L -= my.d;
+  my.R += my.d;
+  my.T -= my.d;
+  my.B += my.d;
+}
