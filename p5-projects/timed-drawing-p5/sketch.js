b4297dab83eba41991c79a62f3be69776abe164e
John Henry Thompson <jht1493@gmail.com>
2023-04-20 22:04:41 -0400

    Update sketch.js

--- p5-projects/timed-drawing-p5/sketch.js
+++ p5-projects/timed-drawing-p5/sketch.js
@@ -1,14 +1,17 @@
-// project files created with p5.vscode "Create p5.js Project"
-// https://editor.p5js.org/jht9629-nyu/sketches/-t2O5JfBr
 // timed-drawing
 
-let a_points = [];
-let a_index = 0;
-let a_startTime;
 let a_lapse = 10; // seconds to re-draw points
 let a_xoffset = 300;
+let a_playback_color = 'yellow';
+let a_draw_color = 'white';
+
 let a_timedDrawing = 0;
-let lapse_slider;
+let a_startTime;
+
+let a_drawings = [];
+let a_drawing_index = 0;
+let a_points = null;
+let a_npoints = 0;
 
 function setup() {
   createCanvas(600, 400);
@@ -21,16 +24,17 @@ function setup() {
   ];
   createDiv(msg.join('<br/>'));
 
-  createButton('startTimedDrawing').mousePressed(startTimedDrawing);
-  createButton('stopTimedDrawing').mousePressed(stopTimedDrawing);
+  createButton('startTimedDraw').mousePressed(startTimedDraw);
+  createButton('stopTimedDraw').mousePressed(stopTimedDraw);
   createButton('clearDrawing').mousePressed(clearDrawing);
   createElement('br');
 
   // createSlider(min, max, oldVal, step)
-  lapse_slider = createSlider(0, 60, a_lapse).input(function () {
+  let lapse_slider = createSlider(0, 60, a_lapse).input(function () {
     a_lapse = this.value();
     // console.log('create_slider aVal ', aVal, 'type', typeof aVal);
     valSpan.html(formatNumber(a_lapse) + '');
+    startTimedDraw();
   });
   // lapse_slider.style('width:300px');
   lapse_slider.style('width:50%');
@@ -52,19 +56,24 @@ function formatNumber(num) {
   return int(num * prec) / prec;
 }
 
-function startTimedDrawing() {
-  console.log('startTimedDrawing');
+function startTimedDraw() {
+  console.log('startTimedDraw');
   a_timedDrawing = 1;
   a_startTime = secsTime();
+  a_npoints = 0;
+  for (let points of a_drawings) {
+    a_npoints += points.length;
+  }
 }
 
-function stopTimedDrawing() {
-  console.log('stopTimedDrawing');
+function stopTimedDraw() {
+  console.log('stopTimedDraw');
   a_timedDrawing = 0;
 }
 
 function clearDrawing() {
   console.log('clearDrawing');
+  a_drawings = [];
   a_points = [];
   a_timedDrawing = 0;
 }
@@ -73,54 +82,52 @@ function mouseDragged() {
   if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) {
     return;
   }
-  // let r = random(10, 50);
+  if (!a_points) {
+    a_points = [];
+    a_drawings.push(a_points);
+  }
   let r = 5;
   let x = mouseX;
   let y = mouseY;
-  // let b = new Point(mouseX, mouseY, r);
   a_points.push({ x, y, r });
 }
 
+function mouseReleased() {
+  console.log('mouseReleased');
+  a_points = null;
+}
+
 function draw_timed() {
-  let n = a_points.length;
+  let n = a_npoints;
   let now = secsTime() - a_startTime;
   let progress = now / a_lapse;
   let stopIndex = int(n * progress);
   if (stopIndex > n) stopIndex = n;
 
-  draw_to(stopIndex);
-
-  if (now > a_lapse) {
-    // a_timedDrawing = 0
-  }
-}
-
-function draw_to(stopIndex) {
-  // console.log('stopIndex_draw stopIndex', stopIndex)
-  stroke('yellow');
-
-  for (let i = 1; i < stopIndex; i++) {
-    let previous = a_points[i - 1];
-    let point = a_points[i];
-    if (!point) {
-      console.log('i', i, 'point', point);
-    }
-    // point.lineFrom(previous, a_xoffset);
-    lineFrom(point, previous, a_xoffset);
-  }
+  draw_to(stopIndex, a_playback_color, a_xoffset);
 }
 
 function draw_points() {
-  stroke(255);
+  // if (!a_points) return;
+  draw_to(Number.MAX_SAFE_INTEGER, a_draw_color, 0);
+}
 
-  for (let i = 1; i < a_points.length; i++) {
-    // a_points[i].move();
-    // a_points[i].show();
-    let previous = a_points[i - 1];
-    let point = a_points[i];
-    // console.log('bub', bub)
-    // point.lineFrom(previous, 0);
-    lineFrom(point, previous, 0);
+function draw_to(stopIndex, color, xoffset) {
+  // console.log('stopIndex_draw stopIndex', stopIndex)
+  // if (!a_points) return;
+  stroke(color);
+  let ipoint = 0;
+  for (let points of a_drawings) {
+    for (let i = 1; i < points.length; i++) {
+      ipoint++;
+      if (ipoint > stopIndex) return;
+      let previous = points[i - 1];
+      let point = points[i];
+      if (!point) {
+        console.log('i', i, 'point', point);
+      }
+      lineFrom(point, previous, xoffset);
+    }
   }
 }
 
@@ -128,34 +135,20 @@ function lineFrom(my, previous, xoffset) {
   line(previous.x + xoffset, previous.y, my.x + xoffset, my.y);
 }
 
-class Point {
-  constructor(x, y, r) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-  }
-
-  lineFrom(previous, xoffset) {
-    line(previous.x + xoffset, previous.y, this.x + xoffset, this.y);
-  }
-
-  move() {
-    this.x = this.x + random(-5, 5);
-    this.y = this.y + random(-5, 5);
-  }
-
-  show() {
-    // stroke(255);
-    // strokeWeight(4);
-    // noFill();
-    ellipse(this.x, this.y, this.r * 2);
-  }
-}
-
 // return seconds since start of sketch
 function secsTime() {
   return millis() / 1000;
 }
+
+// startTimedDraw as slider changes
+// added a_drawings
+// generalize draw_to
+// remove class Point, use object literal for point
+// added funtion lineFrom
+
+// project files created with p5.vscode "Create p5.js Project"
+// https://editor.p5js.org/jht9629-nyu/sketches/-t2O5JfBr
+// timed-drawing
 
 // TRY: use storeItem / getItem to save drawing locally
 // https://p5js.org/reference/#/p5/storeItem
