0245aeaee92ae28e4ffb45c0fd06f406433b96b6
John Henry Thompson <jht1493@gmail.com>
2023-05-08 20:00:21 -0400

    // touchStarted canvas_touchStarted

--- p5-projects/timed-drawing-class/sketch.js
+++ p5-projects/timed-drawing-class/sketch.js
@@ -6,7 +6,7 @@ let my = { width: 640, height: 480 };
 let drawPoints;
 
 function my_init() {
-  my.version = 13;
+  my.version = 14;
   my.save_label = 'plea';
   my.lapse = 5; // seconds to re-draw points
   my.xoffset = my.width / 2;
@@ -111,7 +111,7 @@ function ui_init() {
 
   my.canvas.mouseReleased(canvas_mouseReleased);
   my.canvas.touchEnded(canvas_touchEnded);
-  my.canvas.touchStarted(canvas_touchStarted);
+  // my.canvas.touchStarted(canvas_touchStarted); // my.version = 14;
 }
 
 function mouseDragged() {
