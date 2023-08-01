233dcfc493b81b7d70dad2760766eba063706edb
John Henry Thompson <jht1493@gmail.com>
2023-08-01 07:54:29 -0400

    Update sketch.js

--- p5-ccl/7-sine-wave/sketch.js
+++ p5-ccl/7-sine-wave/sketch.js
@@ -7,22 +7,18 @@ let theta = 0.0; // Start angle at 0
 let amplitude = 75.0; // Height of wave
 let period = 500.0; // How many pixels before the wave repeats
 let da; // Value for incrementing x
-let yvalues; // Using an array to store height values for the wave
 let numa;
 
 function setup() {
   createCanvas(710, 400);
   w = width + 16;
   da = (TWO_PI / period) * xspacing;
-  yvalues = new Array(floor(w / xspacing));
   numa = floor(w / xspacing);
 }
 
 function draw() {
   background(0);
   draw_sine_wave();
-  // calcWave();
-  // renderWave();
 }
 
 function draw_sine_wave() {
@@ -34,27 +30,6 @@ function draw_sine_wave() {
     ellipse(i * xspacing, height / 2 + val, 16, 16);
 
     a += da;
-  }
-}
-
-function calcWave() {
-  // Increment theta (try different values for
-  // 'angular velocity' here)
-  theta += 0.02;
-  // For every x value, calculate a y value with sine function
-  let a = theta;
-  for (let i = 0; i < yvalues.length; i++) {
-    yvalues[i] = sin(a) * amplitude;
-    a += da;
-  }
-}
-
-function renderWave() {
-  noStroke();
-  fill(255);
-  // A simple way to draw the wave with an ellipse at each location
-  for (let x = 0; x < yvalues.length; x++) {
-    ellipse(x * xspacing, height / 2 + yvalues[x], 16, 16);
   }
 }
 
