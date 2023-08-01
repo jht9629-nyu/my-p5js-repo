38d7c9aac63e7adb159cf77054c13ef0201e289c
John Henry Thompson <jht1493@gmail.com>
2023-08-01 08:27:12 -0400

    Update sketch.js

--- p5-ccl/7-sine-wave/sketch.js
+++ p5-ccl/7-sine-wave/sketch.js
@@ -1,19 +1,25 @@
 // https://editor.p5js.org/jht9629-nyu/sketches/-5qcnnr2L
 // sine Amusing lettuce
 
-let xspacing = 16; // Distance between each horizontal location
-let w; // Width of entire wave
-let theta = 0.0; // Start angle at 0
-let amplitude = 75.0; // Height of wave
-let period = 500.0; // How many pixels before the wave repeats
-let da; // Value for incrementing x
-let numa;
+let sw = {};
 
 function setup() {
-  createCanvas(710, 400);
+  createCanvas(390, 600);
+  init_sw();
+}
+
+function init_sw() {
+  sw.xspacing = 16; // Distance between each horizontal location
+  sw.w; // Width of entire wave
+  sw.theta = 0.0; // Start angle at 0
+  sw.amplitude = 75.0; // Height of wave
+  sw.period = 500.0; // How many pixels before the wave repeats
+  sw.da; // Value for incrementing x
+  sw.numa;
+
   w = width + 16;
-  da = (TWO_PI / period) * xspacing;
-  numa = floor(w / xspacing);
+  sw.da = (TWO_PI / sw.period) * sw.xspacing;
+  sw.numa = floor(w / sw.xspacing);
 }
 
 function draw() {
@@ -22,14 +28,17 @@ function draw() {
 }
 
 function draw_sine_wave() {
-  theta += 0.02;
-  let a = theta;
-  for (let i = 0; i < numa; i++) {
-    let val = sin(a) * amplitude;
+  sw.theta += 0.02;
+  let a = sw.theta;
+  for (let i = 0; i < sw.numa; i++) {
+    let val = sin(a) * sw.amplitude;
 
-    ellipse(i * xspacing, height / 2 + val, 16, 16);
+    let x = i * sw.xspacing;
+    let y = height / 2 + val;
 
-    a += da;
+    ellipse(x, y, 16, 16);
+
+    a += sw.da;
   }
 }
 
